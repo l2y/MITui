e70323c9fefa0162039a018f7114c4d8f80d58f0
Jerome Couture <jerome@v1020-wn-192-184.campus-dynamic.uwaterloo.ca>
2016-03-12 13:07:49 -0500

    moved spectrogram tests, added formant plot from praat

--- research/lpctest.m
+++ research/lpctest.m
@@ -1,153 +1,38 @@
 clear all;
 close all;
-[y,fs] = audioread('Hello.wav');
 
-y = y(1:fs*2.5); % trim to 2.5s
+
+%% LPC testing 
+
+[y,fs] = audioread('Hello-short.wav');
+ofs = 60000;
+y = y(ofs:ofs+(fs*0.10));
 t = 0:1/fs:(length(y)-1)/fs;
+w = hamming(length(y));
+yold = y;
+y = y.*w;
 
-%% Narrowband Spectrogram
-% long-time spectrum calculations (~20ms window).
-% emphasize frequency changes in signal.
+%plot(y);
 
-% (0.02 sec * 44100/s = 844);
-
-N = 1024;
-w = hamming(N);
-o = N-16;
+% magnitude spectrum and LP spectral envelope
+YOLD = fft(yold,1024);
+YOLD = YOLD(1:512); % discard negative freq range
 figure(1);
-spectrogram(y,w,o,N,fs,'yaxis');
-colormap(flipud(gray));
-ylim([0 9]);
-title('Narrowband Spectrogram');
+freq = 0:8000/512:8000-1;
+hold on
+%plot(freq,db(abs(Y) / 1024));
 
-%% Wideband Spectrogram - manual plot
-% short time spectrum calculations (~3ms window).
-% emphasize temporal changes in signal (good for speech analysis);
-% (0.003 sec * 44100/s = 132);
+Y = fft(y, 1024);
+Y = Y(1:512);
+plot(freq, db(abs(YOLD/1024)),'b');
+plot(freq,db(abs(Y / 1024)),'r');
 
-N = 512;
-w = hamming(N);
-o = N-16;
-figure(2);
-%spectrogram(y,w,o,N,fs,
-[s,f,t] = spectrogram(y,w,0,N,fs);
-imagesc(t,f,db(abs(s))+1);
-colormap(flipud(gray));
-title('Wideband Spectrogram, Hamming window manual plot');
-axis xy;
-xlabel('time(sec)');
-ylabel('freq(Hz)');
-ylim([0 10000]);
 
-% %% Crazy LPC spectrogram test 
-% N = 512;
-% w = hamming(N);
-% o = N-16;
-% figure(3)
-% [s,f,t] = modspect(y,w,0,N,fs);
-% imagesc(t,f,db(abs(s))+1);
-% colormap(flipud(gray));
-% title('Crazy Spectrogram');
+p = fs/1000 + 5; %order
+[a,g] = lpc(y,p);
+lspec = freqz(g,a,freq,fs);
+plot(freq,db(abs(lspec)),'k');
 
-%% something else
-
-% % Wideband Spectrogram - autoplot
-% N = 512;
-% w = hamming(N);
-% o = N-16;
-% figure(3);
-% spectrogram(y,w,0,N,fs,'yaxis');
-% colormap(flipud(gray));
-% ylim([0 10]);
-% title('Wideband Spectrogram, Hamming window autoplot');
-
-% %% LPC testing 
-% 
-% [y,fs] = audioread('Hello.wav');
-% ofs = 60000;
-% y = y(ofs:ofs+(fs*0.10));
-% t = 0:1/fs:(length(y)-1)/fs;
-% w = hamming(length(y));
-% yold = y;
-% y = y.*w;
-% 
-% %plot(y);
-% 
-% % magnitude spectrum and LP spectral envelope
-% YOLD = fft(yold,1024);
-% YOLD = YOLD(1:512); % discard negative freq range
-% figure(1);
-% freq = 0:8000/512:8000-1;
-% hold on
-% %plot(freq,db(abs(Y) / 1024));
-% 
-% Y = fft(y, 1024);
-% Y = Y(1:512);
-% plot(freq, db(abs(YOLD/1024)),'b');
-% plot(freq,db(abs(Y / 1024)),'r');
-% 
-% 
-% 
-% 
-% 
-% % lpc test
-% p = fs/1000 + 5; %order% %% LPC testing 
-% 
-% [y,fs] = audioread('Hello.wav');
-% ofs = 60000;
-% y = y(ofs:ofs+(fs*0.10));
-% t = 0:1/fs:(length(y)-1)/fs;
-% w = hamming(length(y));
-% yold = y;
-% y = y.*w;
-% 
-% %plot(y);
-% 
-% % magnitude spectrum and LP spectral envelope
-% YOLD = fft(yold,1024);
-% YOLD = YOLD(1:512); % discard negative freq range
-% figure(1);
-% freq = 0:8000/512:8000-1;
-% hold on
-% %plot(freq,db(abs(Y) / 1024));
-% 
-% Y = fft(y, 1024);
-% Y = Y(1:512);
-% plot(freq, db(abs(YOLD/1024)),'b');
-% plot(freq,db(abs(Y / 1024)),'r');
-% 
-% 
-% 
-% 
-% 
-% % lpc test
-% p = fs/1000 + 5; %order
-% [a,g] = lpc(y,p);
-% lspec = freqz(g,a,freq,fs);
-% plot(freq, db(abs(lspec)),'k');
-% 
-% [a,g] = lpc(yold,p);
-% lspec = freqz(g,a,freq,fs);
-% plot(freq,db(abs(lspec)),'g');
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-
-% [a,g] = lpc(y,p);
-% lspec = freqz(g,a,freq,fs);
-% plot(freq, db(abs(lspec)),'k');
-% 
-% [a,g] = lpc(yold,p);
-% lspec = freqz(g,a,freq,fs);
-% plot(freq,db(abs(lspec)),'g');
-% 
-% 
-% 
-% 
-% 
-% 
-% 
+[a,g] = lpc(yold,p);
+lspec = freqz(g,a,freq,fs);
+plot(freq,db(abs(lspec)),'g');
--- research/soundtest.m
+++ research/soundtest.m
@@ -1,9 +1,7 @@
 clear all
 close all
 
-
-[y,fs] = audioread('Hello.wav');
-
+[y,fs] = audioread('Hello-short.wav');
 y = y(1:fs*2.5);
 
 NS = length(y);
@@ -36,4 +34,49 @@ imagesc(t,f,20*log10(abs(s)+1));
 ylim([0 fmax]);
 colormap(gray);
 axis xy;
+
+%% Narrowband Spectrogram
+% long-time spectrum calculations (~20ms window).
+% emphasize frequency changes in signal.
+
+% (0.02 sec * 44100/s = 844);
+
+N = 1024;
+w = hamming(N);
+o = N-16;
+figure(1);
+spectrogram(y,w,o,N,fs,'yaxis');
+colormap(flipud(gray));
+ylim([0 9]);
+title('Narrowband Spectrogram');
+
+%% Wideband Spectrogram - manual plot
+% short time spectrum calculations (~3ms window).
+% emphasize temporal changes in signal (good for speech analysis);
+% (0.003 sec * 44100/s = 132);
+
+N = 512;
+w = hamming(N);
+o = N-16;
+figure(2);
+%spectrogram(y,w,o,N,fs,
+[s,f,t] = spectrogram(y,w,0,N,fs);
+imagesc(t,f,db(abs(s))+1);
+colormap(flipud(gray));
+title('Wideband Spectrogram, Hamming window manual plot');
+axis xy;
+xlabel('time(sec)');
+ylabel('freq(Hz)');
+ylim([0 10000]);
+
+
+%% Wideband Spectrogram - autoplot
+N = 512;
+w = hamming(N);
+o = N-16;
+figure(3);
+spectrogram(y,w,0,N,fs,'yaxis');
+colormap(flipud(gray));
+ylim([0 10]);
+title('Wideband Spectrogram, Hamming window autoplot');
 
