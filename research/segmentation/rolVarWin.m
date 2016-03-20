d25176ac9c4247f10f56319a4005d8f8d6c70fcd
Jerome Couture <jerome@v1020-wn-192-184.campus-dynamic.uwaterloo.ca>
2016-03-20 03:01:08 -0400

    improved vectorization on rolling variance window

--- research/segmentation/rolVarWin.m
+++ research/segmentation/rolVarWin.m
@@ -2,17 +2,16 @@ function [rwinvec] = rolVarWin (input,wlen)
     if mod(wlen,2) == 1
         wlen = wlen+1;
     end
-    
+ 
     if mod(input,2) == 1
         input = [input input(length(input))];
     end
     
     L = length(input);
-    rwinvec = zeros([L-wlen 1]);
+    rwinvec = zeros([1 L-wlen]);
     
     for t = 1:L-wlen
         rwinvec(t) = var(input(t:t+wlen));
-    end
-    
-    rwinvec = [zeros([wlen/2 1])' rwinvec' zeros([wlen/2 1])'];
+    end   
+    rwinvec = [zeros([1 wlen/2]) rwinvec zeros([1 wlen/2])];
 end
