a7cda5b74aefd17112e3f28be2abf7836dffe963
Jerome Couture <jerome@Jeromes-MacBook-Pro.local>
2016-03-20 12:49:00 -0400

    testing upsampling pitch contour to resampled Fs

--- research/segmentation/pitchsegupsampletest.m
+++ research/segmentation/pitchsegupsampletest.m
@@ -1,32 +1,32 @@
 % try to upsample pitch to 44100/4
 
-clear all
-close all
+
 fileID = fopen('howareyoupitch.txt','r');
 formatSpec = '%f %f';
 sizeA = [2 Inf];
 
 A = fscanf(fileID,formatSpec,sizeA);
 fclose(fileID);
+Fs = 44100 / 4;
 
 t = A(1,:);
 pitch = A(2,:);
-
-% looks like normalizing produces same results-ish.
 pitch = normalizesig(pitch,0,1);
 
+[pitchrs,tr] = resample(pitch,t,Fs,'linear');
+smptr = (1:length(tr));
+plot(smptr,pitchrs);
 
+% looks like normalizing produces same results-ish.
 
-% try rolling variance window with ~18 samples on the pitch contour.
-windowSize = 18;
-rwinvec = rolVarWin(pitch,windowSize);
-
-[ax, h1, h2] = plotyy(t, pitch, t, rwinvec, 'scatter', 'plot');
+windowSize = (1.5e4/4);
+rwinvec = rolVarWin(pitchrs,windowSize);
+figure();
+[ax, h1, h2] = plotyy(smptr, pitchrs, smptr, rwinvec, 'scatter', 'plot');
 set(h1, 'CData', [1 0.5 0.5]);
 set(get(ax(1), 'Ylabel'), 'String', 'Frequency (Hz)');
 set(get(ax(2), 'Ylabel'), 'String', 'Rolling Variance');
 xlabel('Time (s)');
 
 legend('Praat pitch contour','18 sample rolling variance window');
-
 
