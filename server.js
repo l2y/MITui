f8007fe01a6ea58b578ab502dde7970f437efa43
Thomas Kelly <tom.kelly93@gmail.com>
2016-03-21 19:35:22 -0400

    only call classifier for i am good

--- server.js
+++ server.js
@@ -125,14 +125,16 @@ function handleRequest(request, response){
 			fs.rename(file.path, form.uploadDir + "\\" + currentWord + "\\" + version + "\\" + recordingCount + ".wav");
 			// exec('C:\\Users\\Cain\\workspace\\MITui\\pitch_detection_matlab.bat ' 
 			// 	+ wavFile + ' ' + pitchTier + ' ' + parsedPitch);
-			var spawn = require('child_process').spawn;
-			ls = spawn('cmd.exe',['/c','word_classification_matlab.bat 4 %1']);
-			ls.stdout.on('data',function(data){
-				console.log('stdout: ' + data);
-			});
-			ls.on('exit', function(code){
-				console.log('child process exited with code ' + code);
-			});
+			if(recordingCount==1 && currentWord=='I Am Good'){
+				var spawn = require('child_process').spawn;
+				ls = spawn('cmd.exe',['/c','word_classification_matlab.bat 4 %1']);
+				ls.stdout.on('data',function(data){
+					console.log('stdout: ' + data);
+				});
+				ls.on('exit', function(code){
+					console.log('child process exited with code ' + code);
+				});
+			}
 			response.end();
 		});
 
--- word_classification_matlab.bat
+++ word_classification_matlab.bat
@@ -1,2 +1,2 @@
 set "wordId=%1"
-matlab -nodesktop -nosplash -minimize -r "cd 'C:\Users\Sarah Kelly\Documents\University\SYDE 461\Code\Website\research\segmentation'; run('classifier_score("%wordId%")');exit" 
+matlab -nodesktop -nosplash -minimize -r "cd 'C:\Users\Sarah Kelly\Documents\University\SYDE 461\Code\Website\research\segmentation'; run('classifier_score_shitty("%wordId%")');exit" 
