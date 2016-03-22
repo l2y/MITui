147059d527b996752c799f84c3f0a899716905fb
Allan Cao <acao737@gmail.com>
2016-03-21 21:19:38 -0400

    Passing parsed pitch dynamically

--- word_classification_matlab.bat
+++ word_classification_matlab.bat
@@ -1,3 +1,3 @@
 set "wordId=%1"
-
-matlab -nodesktop -nosplash -minimize -r "cd 'C:\Users\Cain\workspace\MITui\research\segmentation'; run('classifier_score_shitty("%wordId%")');exit" 
+set "absPath=%2"
+matlab -nodesktop -nosplash -minimize -r "cd 'C:\Users\Cain\workspace\MITui\research\segmentation'; run('classifier_score_shitty("%wordId%", "%absPath%")');exit" 
