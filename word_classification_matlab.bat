7b7efe006c3c0b55e995daeca43cca58fe8907b7
Thomas Kelly <tom.kelly93@gmail.com>
2016-03-21 21:58:11 -0400

    live shit

--- word_classification_matlab.bat
+++ word_classification_matlab.bat
@@ -1,3 +1,3 @@
 set "wordId=%1"
 set "absPath=%2"
-matlab -nodesktop -nosplash -minimize -r "cd 'C:\Users\Cain\workspace\MITui\research\segmentation'; run('classifier_score_shitty("%wordId%", "%absPath%")');exit" 
+matlab -nodesktop -nosplash -minimize -r "cd 'C:\Users\Cain\workspace\MITui\research\segmentation'; run('classifier_score("%wordId%", "%absPath%")');exit" 
